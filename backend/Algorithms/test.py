98f9f94b05444e5ed50ff4f35e39f9d7b98d0533
youssef <youtarek2002@gmail.com>
2024-10-28 22:54:58 +0300

    updating the report

--- backend/Algorithms/test.py
+++ backend/Algorithms/test.py
@@ -1,8 +1,35 @@
 from it_dfs import IT_DFS
 from Astar import AStar
+from bfs import BFS
+from dfs import DFS
 
-it_dfs=IT_DFS("804716352")
-print(it_dfs.run())
-print(it_dfs.get_info())
-# visited = {"alone": 15}
-# print (visited.get("alone"))
+# IT_DFS
+it_dfs = IT_DFS("041586732","012345678")
+# it_dfs.run()
+print("IT_DFS Path:", it_dfs.run())
+print(f"IT_DFS Info: {it_dfs.get_info()}")
+
+# AStar
+a_star = AStar("041586732","012345678","Euclidean")
+# a_star.run()
+print("AStar Euclidean Path:", a_star.run())
+print(f"AStar Euclidean Info: {a_star.get_info()}")
+
+# AStar
+a_star = AStar("041586732","012345678","Manhattan")
+print("AStar Manhattan Path:", a_star.run())
+# a_star.run()
+print(f"AStar Manhattan Info: {a_star.get_info()}")
+
+
+# BFS
+bfs = BFS("041586732","012345678")
+print("BFS Path:", bfs.run())
+# bfs.run()
+print(f"BFS Info: {bfs.get_info()}")
+
+# DFS
+dfs = DFS("041586732","012345678")
+print("DFS Path:", dfs.run())
+# dfs.run()
+print(f"DFS Info: {dfs.get_info()}")
