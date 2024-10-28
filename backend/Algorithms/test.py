44e8d6b2876118a58e483366ceb80485d9258bcc
asserelzeki6 <asserelzeki@gmail.com>
2024-10-28 21:43:43 +0300

    uploading test.py

---
+++ backend/Algorithms/test.py
@@ -0,0 +1,8 @@
+from it_dfs import IT_DFS
+from Astar import AStar
+
+it_dfs=IT_DFS("804716352")
+print(it_dfs.run())
+print(it_dfs.get_info())
+# visited = {"alone": 15}
+# print (visited.get("alone"))
